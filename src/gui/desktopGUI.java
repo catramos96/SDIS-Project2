ed02693a816dd2487d375c6b1b0d2efd48f9306f
Pedro Dias <up201404178@fe.up.pt>
2017-05-25 01:04:29 +0100

    refactor gui

--- src/gui/desktopGUI.java
+++ src/gui/desktopGUI.java
@@ -6,16 +6,24 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTree;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JRadioButton;
+import javax.swing.JTextPane;
 
 public class desktopGUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -23,6 +31,8 @@ public class desktopGUI extends JFrame {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				
+				
 				try {
 					desktopGUI frame = new desktopGUI();
 					frame.setVisible(true);
@@ -37,69 +47,123 @@ public class desktopGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public desktopGUI() {
+		
+		setDefaultTheme();
+		
+		// Frame Specs
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 475, 620);
+		setBounds(100, 100, 286, 620);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JLabel lblTrackerIp = new JLabel("Tracker IP");
+		lblTrackerIp.setBounds(10, 11, 74, 14);
+		contentPane.add(lblTrackerIp);
+		
 		textField = new JTextField();
-		textField.setBounds(223, 77, 114, 19);
+		textField.setText("127.0.0.1");
+		textField.setBounds(70, 8, 179, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
+		JLabel lblPort = new JLabel("Port");
+		lblPort.setBounds(10, 36, 46, 14);
+		contentPane.add(lblPort);
+		
 		textField_1 = new JTextField();
-		textField_1.setBounds(223, 108, 114, 19);
+		textField_1.setText("8080");
+		textField_1.setBounds(70, 36, 179, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
-		JButton btnRegister = new JButton("Register");
-		btnRegister.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnRegister.setBounds(98, 139, 104, 25);
-		contentPane.add(btnRegister);
+		JButton btnNewButton = new JButton("Launch Tracker");
+		btnNewButton.setBounds(20, 67, 232, 23);
+		contentPane.add(btnNewButton);
 		
-		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setBounds(113, 79, 89, 15);
-		contentPane.add(lblUsername);
+		textField_2 = new JTextField();
+		textField_2.setText("127.0.0.1");
+		textField_2.setBounds(70, 112, 179, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
 		
-		JLabel lblPass = new JLabel("Pass:");
-		lblPass.setBounds(113, 110, 70, 15);
-		contentPane.add(lblPass);
+		JLabel lblPeerIp = new JLabel("Peer IP");
+		lblPeerIp.setBounds(10, 115, 46, 14);
+		contentPane.add(lblPeerIp);
 		
-		JLabel lblLoginregister = new JLabel("Login/Register");
-		lblLoginregister.setBounds(162, 50, 110, 15);
-		contentPane.add(lblLoginregister);
+		textField_3 = new JTextField();
+		textField_3.setText("8081");
+		textField_3.setBounds(70, 142, 179, 23);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
 		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setBounds(223, 139, 94, 25);
-		contentPane.add(btnLogin);
+		JLabel lblPort_1 = new JLabel("Port");
+		lblPort_1.setBounds(10, 146, 46, 14);
+		contentPane.add(lblPort_1);
 		
-		JTree tree = new JTree();
-		tree.setBounds(34, 249, 155, 270);
-		contentPane.add(tree);
+		JButton btnLaunchPeer = new JButton("Launch Peer");
+		btnLaunchPeer.setBounds(20, 176, 232, 20);
+		contentPane.add(btnLaunchPeer);
 		
-		JLabel lblFileManager = new JLabel("File Manager");
-		lblFileManager.setBounds(34, 223, 104, 15);
-		contentPane.add(lblFileManager);
+		textField_4 = new JTextField();
+		textField_4.setBounds(70, 264, 179, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
 		
-		JButton btnNewButton_1 = new JButton("Download");
-		btnNewButton_1.setBounds(262, 531, 117, 25);
+		JLabel lblPath = new JLabel("Path");
+		lblPath.setBounds(10, 267, 46, 14);
+		contentPane.add(lblPath);
+		
+		ButtonGroup group = new ButtonGroup();
+		
+		JRadioButton rdbtnSend = new JRadioButton("Backup");
+		rdbtnSend.setBounds(70, 309, 109, 23);
+		group.add(rdbtnSend);
+		
+		JRadioButton rdbtnDelete = new JRadioButton("Delete");
+		rdbtnDelete.setBounds(70, 335, 109, 23);
+		group.add(rdbtnDelete);
+		
+		JRadioButton rdbtnRestore = new JRadioButton("Restore");
+		rdbtnRestore.setBounds(70, 361, 109, 23);
+		group.add(rdbtnRestore);
+		
+		contentPane.add(rdbtnSend);
+		contentPane.add(rdbtnDelete);
+		contentPane.add(rdbtnRestore);
+		
+		
+		
+		JButton btnNewButton_1 = new JButton("Start");
+		btnNewButton_1.setBounds(10, 396, 239, 23);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnLoadFile = new JButton("Upload");
-		btnLoadFile.setBounds(55, 531, 117, 25);
-		contentPane.add(btnLoadFile);
-		
-		JTree tree_1 = new JTree();
-		tree_1.setBounds(223, 249, 181, 270);
-		contentPane.add(tree_1);
-		
-		JLabel lblStoredFiles = new JLabel("Stored Files");
-		lblStoredFiles.setBounds(223, 223, 114, 15);
-		contentPane.add(lblStoredFiles);
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(255, 430, -244, 140);
+		contentPane.add(textPane);
+	}
+	/**
+	 * Set default theme
+	 */
+	
+	public void setDefaultTheme() {
+		// Defines the theme
+		 try {
+	            // Set System L&F
+	        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	    } 
+	    catch (UnsupportedLookAndFeelException e) {
+	       // handle exception
+	    }
+	    catch (ClassNotFoundException e) {
+	       // handle exception
+	    }
+	    catch (InstantiationException e) {
+	       // handle exception
+	    }
+	    catch (IllegalAccessException e) {
+	       // handle exception
+	    }
 	}
 }
