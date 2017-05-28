4b1a7a121984daf4e739fe68690b89c9d10afe43
Cat <catramos96@gmail.com>
2017-05-28 20:06:56 +0100

    Tracker data doc

--- src/tracker/TrackerData.java
+++ src/tracker/TrackerData.java
@@ -7,16 +7,27 @@ import java.io.Serializable;
 import java.util.*;
 import java.util.Map.Entry;
 
+/**
+ * Serializable class to save the information about the files in backup
+ */
 public class TrackerData implements Serializable
 {
     private static final long serialVersionUID = 1L;
     private HashMap<String,HashSet<Subscriber>> DHT = null;
 
+    /**
+     * Constructor that initializes the DHT
+     */
     public TrackerData()
     {
         DHT = new HashMap<String,HashSet<Subscriber>>();
     }
 
+    /**
+     * Function that inserts a new subscriber that has saved the chunk of a backup
+     * @param key of the chunk
+     * @param s information of the peer
+     */
     public synchronized void putDHT(String key, Subscriber s){
         if(DHT.containsKey(key)){
             DHT.get(key).add(s);
@@ -30,6 +41,14 @@ public class TrackerData implements Serializable
         }
     }
 
+    /**
+     * Function that returns peersN Subscribers holding a chunk with key
+     * and with pagination p
+     * @param key of chunk
+     * @param peersN number of peers to return
+     * @param pagination of query
+     * @return
+     */
     public synchronized ArrayList<Subscriber> getDHT(String key, int peersN, int pagination){
         ArrayList<Subscriber> subs = new ArrayList<Subscriber>();
         Iterator<Subscriber> it;
@@ -57,12 +76,23 @@ public class TrackerData implements Serializable
         return subs;
     }
 
+    /**
+     * Function that checks a chunk with key and returns the number os Subscribers in the HashSet of that ket
+     * @param key of chunk
+     * @return Number of peers in the hashSet
+     */
     public synchronized int checkDHT(String key){
         if(DHT.containsKey(key))
             return DHT.get(key).size();
         return 0;
     }
 
+    /**
+     * Function that removes a subscriber from the chunk key entry
+     * @param key of chunk
+     * @param s subscriber to remove
+     * @return Number of chunks that remained
+     */
     public synchronized int remSubscriberDHT(String key, Subscriber s){
     	if(DHT.containsKey(key)){
 	        HashSet<Subscriber> tmp = DHT.get(key);
@@ -77,6 +107,10 @@ public class TrackerData implements Serializable
         return 0;
     }
 
+    /**
+     * Function that deletes de entry key of a chunk
+     * @param key of the chunk
+     */
     public synchronized void deleteDHT(String key){
         if(DHT.containsKey(key)) {
             DHT.remove(key);
