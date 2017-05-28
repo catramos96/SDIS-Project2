a07fc7ba53974402bb616ac9b122a19bdbd8ab0a
Inês <gomes.inesisabel@gmail.com>
2017-05-28 10:49:21 +0100

    removed from git out files

--- out/production/SDIS-Project2/test2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-start rmiregistry
-timeout /t 3
-start java -Djavax.net.ssl.trustStore=truststore -Djavax.net.ssl.trustStorePassword=123456 -Djavax.net.ssl.keyStore=server.keys -Djavax.net.ssl.keyStorePassword=123456 tracker.PeerTracker 8000
-timeout /t  2
-start java -Djavax.net.ssl.trustStore=truststore -Djavax.net.ssl.trustStorePassword=123456 -Djavax.net.ssl.keyStore=client.keys -Djavax.net.ssl.keyStorePassword=123456 peer.FileSharing 8001 rmi1 127.0.0.1:8000
-timeout /t  2
-exit
